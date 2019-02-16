29bdda1197d144959c5b1009a37a66f0757b028a
Paul Ashour <paul.ashour@gmail.com>
2019-02-16 17:28:10 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -18,7 +18,7 @@ class App < Sinatra::Base
     "Goodbye, #{@user_name}."
   end
   
-  get "/multiplier/:num1" do
+  get "/multiply/:num1" do
     @product = params[:num1].to_i * 3
     @product
     200
